14fcf5506471daabcbbf6ccdfa4431e1dbe7a3e3
migueldoctor <mikydoc_sev@hotmail.com>
2020-03-11 15:44:25 +0100

    Updated port as property for the controller to see how load balancing works

--- src/main/java/com/kesizo/microservices/client/controller/EchoServiceController.java
+++ src/main/java/com/kesizo/microservices/client/controller/EchoServiceController.java
@@ -1,19 +1,21 @@
 package com.kesizo.microservices.client.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class EchoServiceController {
 
+    @Value("${server.port}")
+    private String port;
 
     @RequestMapping(value = "/")
     public String echoService(){
-        return "Echo Service ";
+        return "Echo Service " + "from port "+port;
     }
 
     @GetMapping("/{id}")
     public String echoService(@PathVariable String id){
-
-        return id;
+        return id + " from port "+port;
     }
 }
